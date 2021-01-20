398fc1a86afa13f6819d5316ce7ee213fdb99753
PaoloPerroneJdk <paoloperrone1984@hotmail.com>
2021-01-20 16:53:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-aikwlk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-aikwlk)
---
+++ index.ts
@@ -0,0 +1,35 @@
+import { from ,Observable} from "rxjs";
+import { filter, reduce } from "rxjs/operators";
+import {invoices} from './data.json'
+
+type Invoice = {
+  id : string;
+  customer: string;
+  date:Date
+  amount:number;
+  state: string;
+}
+
+
+
+const observable = from  (invoices)
+
+observable
+.pipe(
+  filter((item:Invoice) => item.customer === 'Paolo Perrone')
+).subscribe(item => console.log(item.amount));
+
+const observable2 = from (invoices)
+
+observable
+.pipe(
+  filter((item : Invoice) => {
+    const timeStamp = item.date;
+    const month = new Date(timeStamp).getMonth()+1 /* mettiamo +1 perchè i mesi partono da zero */
+    return month === 12;
+  }),
+  reduce( (acc : number , item : Invoice) =>{
+   
+    return acc +item.amount;
+  },0 )
+).subscribe(val => console.log(`Il totale fatturato di dicembre è : ${val}`))
